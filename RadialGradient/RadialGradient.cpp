4ddcb60235c56ec6e674381784fd95b9f9d13a1a
TheideaLAXE <andrej.miovci@gmail.com>
2025-09-30 19:10:50 +0200

    Changing Variable Names 2.0

--- RadialGradient/RadialGradient.cpp
+++ RadialGradient/RadialGradient.cpp
@@ -1,13 +1,14 @@
 #include <fstream>
 #include <cmath>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 const int img_wh=1023;
-const int img_wh2=img_wh/2;
-const double Dist=sqrt((img_wh2*img_wh2)+(img_wh2*img_wh2));
-const double Hue=255.0/(Dist+2);
+const int img_hlf=img_wh/2;
+const double Dist=sqrt((img_hlf*img_hlf)+(img_hlf*img_hlf));
+const double Hue=255.0/(Dist+20);
 
 struct Pixel {
     int r, g, b;
@@ -25,10 +26,10 @@ int main() {
 
     for (int i = 0; i < img_wh; i++) {
         for (int j = 0; j < img_wh; j++) {
-            double dist=sqrt((i-img_wh2)*(i-img_wh2)+(j-img_wh2)*(j-img_wh2));
-            int l = Hue*(Dist-dist);
-            //int d=dist*tempImg;
-            img.Image[i][j].r = l%255;
+            double dist=sqrt((i-img_hlf)*(i-img_hlf)+(j-img_hlf)*(j-img_hlf));
+            int len = Hue*(Dist-dist);
+            //int len2=dist*tempImg;
+            img.Image[i][j].r = len%255;
             img.Image[i][j].g = 0;
             img.Image[i][j].b = 0;
         }
@@ -41,6 +42,6 @@ int main() {
         }
         out << "\n";
     }
-
+    cout << "Done printing!";
     return 0;
 }
